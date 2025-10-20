07be8fab260b2a40b844da85d9a0b67f5e5e368d
error01121 <sanzhar.kuderov@gmail.com>
2025-10-20 21:36:03 +0500

    Add files via upload

---
+++ assets/js/swiper.js
@@ -0,0 +1,24 @@
+const projectSwiper = new Swiper('.project-home__row', {
+  slidesPerView: 'auto',
+  freeMode: true,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  }
+});
+const productSwiper = new Swiper('.product-home__row', {
+  slidesPerView: 'auto',
+  freeMode: true,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  }
+});
+const instagramSwiper = new Swiper('.instagram-home__row', {
+  slidesPerView: 'auto',
+  freeMode: true,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  }
+});
